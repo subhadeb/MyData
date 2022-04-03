9ee44a743ef4baa36c7dc97932e62e15dcff49a4
Subha Deb <62798027+subhadeb@users.noreply.github.com>
2022-04-03 21:45:35 +0530

    Update MyProgram.cs

--- MyProgram.cs
+++ MyProgram.cs
@@ -16,13 +16,27 @@ namespace Rextester
         {
            List<Employee> employees = new List<Employee>();
             employees.Add(new Employee() { FirstName = "John", Age = 29 });
+            employees.Add(new Employee() { FirstName = "Jane", Age = 39 });
             employees.Add(new Employee() { FirstName = "Matts", Age = 25 });
-            employees.Add(new Employee() { FirstName = "Eliza", Age = 30 });
             employees.Add(new Employee() { FirstName = "Plank", Age = 29 });
+            employees.Add(new Employee() { FirstName = "Eliza", Age = 42 });
+            employees.Add(new Employee() { FirstName = "Maria", Age = 18 });
+            employees.Add(new Employee() { FirstName = "James", Age = 26 });
             
-            var firstName = employees.FirstOrDefault().FirstName;
             //Find The First Name of the Highest Aged Employee in Capitalized format using LINQ(It should output ELIZA)
+            var firstName = employees.FirstOrDefault().FirstName;
+            
             Console.WriteLine(firstName);
+
+            //Find All employees between the age 20-30
+			Console.WriteLine();
+            var employeesFiltered = employees.ToList();
+            
+            Console.WriteLine("Filtered Employees: ");
+            foreach (var employee in employeesFiltered)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.Age);
+            }
         }
     }
 }
